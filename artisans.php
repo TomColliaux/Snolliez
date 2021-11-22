a83053b7690a5f4f92b11e33a1ea002699b7c406
colliaux <tomolivier.colliaux@epsi.fr>
2021-11-22 16:15:18 +0100

    Creation of the website

---
+++ artisans.php
@@ -0,0 +1,153 @@
+
+
+<style>
+
+.header{
+    background-color: #0A0A0A;
+    height: 15rem;
+}
+
+.header-text h3{
+    color: #FFFFFF;
+    padding-top: 7rem;
+    text-align: center;
+    font-family: Dubai;
+    text-decoration: underline;
+    text-underline-color: #FFFFFF;
+}
+
+.twitchSection{
+    padding-top: 0.2rem;
+    background: linear-gradient(to bottom right, #BDB2FF,#D62828);
+    padding-bottom: 0.2rem;
+}
+
+.twitchDiv{
+    background: url("./img/twitch.jpg") 100% 100% no-repeat padding-box;
+    background-size: cover;
+    height: auto;
+    width: 100%;
+
+}
+.containLeft1{
+    background: url("./img/calendar.jpg") 100% 100% no-repeat padding-box;
+    background-size: cover;
+    width: 100%;
+    padding-bottom: 2rem;
+    padding-top: 2rem;
+    height: 240px;
+    color: #FFFFFF;
+
+}
+.containLeft2{
+    background: url("./img/discord.jpg") 100% 100% no-repeat padding-box;
+    background-size: cover;
+    width: 100%;
+    padding-bottom: 2rem;
+    padding-top: 2rem;
+    height: 240px;
+    color: #FFFFFF;
+    margin-top:0.2rem;
+
+}
+.containLeft3{
+    background: url("./img/help.jpg") 100% 100% no-repeat padding-box;
+    background-size: cover;
+    width: 100%;
+    padding-bottom: 2rem;
+    padding-top: 2rem;
+    height: 240px;
+    color: #FFFFFF;
+    margin-top:0.2rem;
+
+}
+.containRight1{
+    height: auto;
+    width: 100%;
+    color: #FFFFFF;
+}
+.footer{
+    background-color: #0A0A0A;
+    height: 15rem;
+}
+.chatSnolliez{
+    height: 100%;
+}
+
+</style>
+
+
+
+
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<main id="primary" class="site-main">
+
+    <header>
+    <div class="container-fluid header">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-3 header-text">
+                    <h3>ACCUEIL</h3>
+                </div>
+
+                <div class="col-md-3 header-text">
+                    <h3>TWITCH</h3>
+                </div>
+
+                <div class="col-md-3 header-text">
+                    <h3>YOUTUBE</h3>
+                </div>
+
+                <div class="col-md-3 header-text">
+                    <h3>TIKTOK</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+    </header>
+
+        <div class="twitchSection">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2 discordCalendrierSoutien">
+                    <div class="col-md-12 containLeft1">
+                        <h3>Calendrier de Snolliez</h3>
+                        <a href="">Calendrier</a>
+                    </div>
+                    <div class="col-md-12 containLeft2">
+                        <h3>Discord Communautaire</h3>
+                        <a href="">Discord</a>
+                    </div>
+                    <div class="col-md-12 containLeft3">
+                        <h3>Me soutenir</h3>
+                        <a href="">Khalass Paypal</a>
+                    </div>
+
+                </div>
+
+                <div class=" col-md-7 twitchDiv">
+
+                </div>
+
+                <div class="col-md-3 reseauxYoutubeTiktokTwitch">
+                    <div class="col-md-12 containRight1">
+                        <iframe
+                                id="chat_embed"
+                                src="https://www.twitch.tv/embed/snolliez/chat?parent=http://localhost/perso/artisans.php"
+                                height="500"
+                                width="350">
+                        </iframe>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+
+    <footer>
+        <div class="container-fluid footer">
+
+        </div>
+
+    </footer>
